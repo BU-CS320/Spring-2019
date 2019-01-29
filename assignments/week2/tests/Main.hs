877426132e58e0f65fa7c670cf26d5e714ef3789
Mark <lemay@bu.edu>
2019-01-28 22:38:58 -0500

    week2

---
+++ assignments/week2/tests/Main.hs
@@ -0,0 +1,9 @@
+module Main where
+import Hw
+
+import Test.Tasty (defaultMain, testGroup)
+import Test.Tasty.HUnit (assertEqual, assertBool, testCase)
+
+main = defaultMain unitTests
+
+unitTests = error "you downloaded the assignment before tests were posted, try 'git pull upstream master' to see if the tests are posted now"
