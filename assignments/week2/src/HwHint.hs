9c16708a91923cbbdf8737e783305be1a69a9042
Mark Lemay <lemay@bu.edu>
2019-01-30 11:04:37 -0500

    Create HwHint.hs

---
+++ assignments/week2/src/HwHint.hs
@@ -0,0 +1,8 @@
+module HwHint where
+
+-- you will likely need to define the following helper function
+
+-- Concatenate two lists, make sure it works
+-- if either or both are empty
+append :: (List a) -> (List a) -> (List a)
+append  _ _  = undefined
