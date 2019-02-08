1989058c3049aba3bf72503c3d4c0eb2697a45c5
chantisnake <zcsx.ok@outlook.com>
2019-02-08 11:27:04 -0500

    move intTests to a sperate file

--- assignments/week3/tests/Main.hs
+++ assignments/week3/tests/Main.hs
@@ -6,10 +6,8 @@ module Main where
 
 import Hw02
 import System.Environment
-import Data.List
-import Test.Tasty (defaultMain, testGroup, TestTree)
-import Test.Tasty.HUnit (assertEqual, assertBool, testCase, (@=?))
-import Test.Tasty.QuickCheck as QC
+import Test.Tasty (defaultMain, testGroup)
+import IntTests (intTests)
 
 main = 
     do 
@@ -20,44 +18,8 @@ main =
         unsetEnv "TASTY_TIMEOUT"
         unsetEnv "TASTY_QUICKCHECK_TESTS"
         unsetEnv "TASTY_QUICKCHECK_MAX_SIZE"
-        
 
 allTests = testGroup "all tests" [
         intTests
     ]
-
----- Test for Integers ----
-intTests = testGroup "tests for Integer function" 
-    [
-        fibTests,
-        gcdTests
-    ]
-
--- Test for fib
-fibTests = testGroup "tests for fib function" 
-    [
-        QC.testProperty "fib n + fib (n+1) = fib (n+2)" $ 
-            \n -> n > 0 QC.==> fib n + fib (n+1) == fib (n+2)
-    ]
-
-
--- Tests for gcd 
-
-gcdDivisorTest = QC.testProperty "GCD needs to divides both number" $
-    \m n -> m > 0 && n > 0 QC.==> (m `mod` Hw02.gcd m n == 0) && (n `mod` Hw02.gcd m n == 0)
-
-factors n = [x | x <- [1..n], x `mod` n == 0]
-commonFactor :: Integer -> Integer -> [Integer]
-commonFactor n m = factors n `intersect` factors m
-
-gcdGreatestTest = QC.testProperty "GCD cannot be smaller than any common factors" $ 
-    \m n -> 
-        m > 0 && n > 0 QC.==> all (\fac -> fac <= Hw02.gcd m n) (commonFactor n m)
-
-gcdTests = testGroup "tests for gcd function"
-        [
-            gcdDivisorTest,
-            gcdGreatestTest
-        ]
-
 
