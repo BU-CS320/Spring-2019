0943a75c65ef3a0d118a0732361ff0f370fdb595
chantisnake <zcsx.ok@outlook.com>
2019-02-11 18:45:07 -0500

    update error messages

--- assignments/week3/tests/IntTests.hs
+++ assignments/week3/tests/IntTests.hs
@@ -1,6 +1,6 @@
 module IntTests where 
 
-import Hw02
+import Hw02 (fib, gcd)
 import Data.List
 import Test.Tasty (testGroup, TestTree)
 import Test.Tasty.HUnit (assertEqual, testCase, (@=?))
@@ -9,21 +9,21 @@ import Test.Tasty.QuickCheck as QC
 -- Test for fib
 fibTests = testGroup "tests for fib function" 
     [
-        QC.testProperty "fib n + fib (n+1) = fib (n+2)" $ 
-            \n -> n > 0 QC.==> fib n + fib (n+1) == fib (n+2)
+        QC.testProperty "forall n, fib n + fib (n+1) = fib (n+2)" $ 
+            \n -> n >= 0 QC.==> fib n + fib (n+1) == fib (n+2)
     ]
 
 
 -- Tests for gcd 
 
-gcdDivisorTest = QC.testProperty "GCD needs to divides both number" $
+gcdDivisorTest = QC.testProperty "forall m n larger than 0, gcd of m n divids both n and m" $
     \m n -> m > 0 && n > 0 QC.==> (m `mod` Hw02.gcd m n == 0) && (n `mod` Hw02.gcd m n == 0)
 
 factors n = [x | x <- [1..n], x `mod` n == 0]
 commonFactor :: Integer -> Integer -> [Integer]
 commonFactor n m = factors n `intersect` factors m
 
-gcdGreatestTest = QC.testProperty "GCD cannot be smaller than any common factors" $ 
+gcdGreatestTest = QC.testProperty "forall m n larger than 0, gcd of m n is " $ 
     \m n -> 
         m > 0 && n > 0 QC.==> all (\fac -> fac <= Hw02.gcd m n) (commonFactor n m)
 
