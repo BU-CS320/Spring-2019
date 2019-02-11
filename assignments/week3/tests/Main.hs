b8ebb1d2dd5103a5d911a1f1c8b3a78440455230
chantisnake <zcsx.ok@outlook.com>
2019-02-11 18:45:48 -0500

    move infrastructure to testBase

--- assignments/week3/tests/Main.hs
+++ assignments/week3/tests/Main.hs
@@ -1,17 +1,22 @@
-{-# LANGUAGE MultiParamTypeClasses #-}
-{-# LANGUAGE StandaloneDeriving #-}
-{-# LANGUAGE FlexibleInstances #-}
-
 module Main where
 
-import Hw02
-import System.Environment
-import Test.Tasty (defaultMain, testGroup)
-import IntTests (intTests)
 
+import IntTests (intTests)
+import ListTests (listTests)
+import OrderingTests (orderingTests)
+import SetTests (setTests)
+import StudentTests (studentTests)
+import System.Environment
+
+import Hw02
+import Test.Tasty (defaultMain, testGroup, TestTree)
+import Test.Tasty.HUnit (assertEqual, assertBool, testCase, (@=?))
+import Test.Tasty.QuickCheck (testProperty,Arbitrary, oneof,arbitrary )
+
+  
 main = 
     do 
-        setEnv "TASTY_TIMEOUT" "5s"
+        -- setEnv "TASTY_TIMEOUT" "5s"
         setEnv "TASTY_QUICKCHECK_TESTS" "20"
         setEnv "TASTY_QUICKCHECK_MAX_SIZE" "50"
         defaultMain allTests
@@ -20,6 +25,10 @@ main =
         unsetEnv "TASTY_QUICKCHECK_MAX_SIZE"
 
 allTests = testGroup "all tests" [
-        intTests
+        intTests,
+        listTests,
+        orderingTests,
+        setTests,
+        studentTests
     ]
 
---
+++ assignments/week3/tests/TestBase.hs
@@ -0,0 +1,44 @@
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE  DeriveDataTypeable, DeriveGeneric #-}
+
+module TestBase where 
+
+import Hw02
+import Set
+import GHC.Generics (Generic)
+import Data.Typeable (Typeable)
+import Test.QuickCheck.Arbitrary.Generic
+
+
+-- derive all the type class that is needed
+
+-- type class for student
+deriving instance (Typeable Student)
+deriving instance (Generic Student)
+deriving instance (Eq Student)
+
+instance Arbitrary Student where
+  arbitrary = genericArbitrary
+  shrink = genericShrink
+
+  
+-- type class for list
+deriving instance (Typeable a) => (Typeable (List a))
+deriving instance (Generic a) => (Generic (List a))
+deriving instance (Eq a) => (Eq (List a))
+
+instance (Generic a, Arbitrary a) => Arbitrary (List a) where
+  arbitrary = genericArbitrary
+  shrink = genericShrink
+
+-- type class for comparasion
+deriving instance Eq Comparison
+
+-- type class for set
+instance (Eq a) => (Set a) where 
+    set1 == set2 = elems set1 == elem set2
+-- because set have an ordering inside of them,
+-- therefore there cannot be any Arbitrary defined on set
+-- we need to generate a arbitrary list and convert them into set
