43d294771fdc8881fe076a13a3401f20d2383984
chantisnake <zcsx.ok@outlook.com>
2019-02-17 10:46:57 -0500

    fix bug mentioned in @516

--- assignments/week4/tests/HigherOrderTest.hs
+++ assignments/week4/tests/HigherOrderTest.hs
@@ -40,7 +40,7 @@ zipTest1 = QC.testProperty "For all Lists, zipWith should act like Prelude's zip
 zipTest2 = QC.testProperty "For all Lists, zipWith should act like Prelude's zipWith" $ 
     \la lb -> (Prelude.zipWith (*) (la::[Integer]) (lb::[Integer])) == (HigherOrderProblems.zipWith (*) (la::[Integer]) (lb::[Integer]))
 zipTest3 = QC.testProperty "For all Lists, zipWith should act like Prelude's zipWith" $ 
-    \la lb -> (Prelude.zipWith (mod) (la::[Integer]) (lb::[Integer])) == (HigherOrderProblems.zipWith (mod) (la::[Integer]) (lb::[Integer]))
+    \la lb -> (Prelude.zipWith (++) la lb) == (HigherOrderProblems.zipWith (++) (la::[String]) (lb::[String]))
 zipTest4 = QC.testProperty "For all Lists, zipWith should act like Prelude's zipWith" $ 
     \la lb -> (Prelude.zipWith (max) (la::[Integer]) (lb::[Integer])) == (HigherOrderProblems.zipWith (max) (la::[Integer]) (lb::[Integer]))
 zipWithTests = testGroup "Tests for zipWith" [
@@ -58,7 +58,7 @@ listAdderTests = testGroup "Tests for listAdder func" [
     testCase "listAdder should work" $ assertEqual [] ([10]) (listAdder [1] [9]),
     testCase "listAdder should work" $ assertEqual [] ([3,5,7]) (listAdder [1,2,3] [2,3,4]),
     testCase "listAdder should work" $ assertEqual [] ([11,7]) (listAdder [2,3] [9,4,2,9]),
-    testCase "listAdder should work" $ assertEqual [] ([3,5,7]) (listAdder [1,5,8,6,3] [2,9,8])
+    testCase "listAdder should work" $ assertEqual [] ([3,14,16]) (listAdder [1,5,8,6,3] [2,9,8])
     ]
 
 listMaxerTests = testGroup "Tests for listAdder func" [
@@ -93,8 +93,8 @@ petTests = testGroup "Tests for Pet datatype" [
     QC.testProperty "For all Dogs, they are always happy" $
         \temp -> isHappy (mkDog "good pupper") temp,
         
-    QC.testProperty "For all Cats, they are conditionally happy" $
-        \temp thresh -> (isHappy (mkCat "good neko" (\t -> t<thresh))  temp) && temp<thresh,
+    QC.testProperty "For all temp and thresh, we make a cat that is happy if the temp is under thresh, then either the cat is happy or temp is larger or equal to thresh" $
+        \temp thresh -> (isHappy (mkCat "good neko" (\t -> t<thresh))  temp) || temp>=thresh,
         
     QC.testProperty "For all Cats, you should name them properly" $
         \n -> n == name (mkCat n (\_ -> True)),
@@ -111,7 +111,13 @@ teamTests = testGroup "tests for team functions" [
     testCase "yearAndTeamWithTotalGames should work" $ assertEqual [] (yearAndTeamWithTotalGames footballExampleStats) ([(1960,"Patriots",14),(1961,"Patriots",14),(1962,"Patriots",14),(1963,"Patriots",14),(1964,"Patriots",14),(1965,"Patriots",14),(1966,"Patriots",14),(1967,"Patriots",14),(1968,"Patriots",14),(1969,"Patriots",14),(1970,"Patriots",14),(1971,"Patriots",14),(1972,"Patriots",14),(1973,"Patriots",14),(1974,"Patriots",14),(1975,"Patriots",14),(1976,"Patriots",14),(1977,"Patriots",14),(1978,"Patriots",16),(1979,"Patriots",16),(1980,"Patriots",16),(1981,"Patriots",16),(1982,"Patriots",9),(1983,"Patriots",16),(1984,"Patriots",16),(1985,"Patriots",16),(1986,"Patriots",16),(1987,"Patriots",15),(1988,"Patriots",16),(1989,"Patriots",16),(1990,"Patriots",16),(1991,"Patriots",16),(1992,"Patriots",16),(1993,"Patriots",16),(1994,"Patriots",16),(1995,"Patriots",16),(1996,"Patriots",16),(1997,"Patriots",16),(1998,"Patriots",16),(1999,"Patriots",16),(2000,"Patriots",16),(2001,"Patriots",16),(2002,"Patriots",16),(2003,"Patriots",16),(2004,"Patriots",16),(2005,"Patriots",16),(2006,"Patriots",16),(2007,"Patriots",16),(2008,"Patriots",16),(2009,"Patriots",16),(2010,"Patriots",16),(2011,"Patriots",16),(2012,"Patriots",16),(2013,"Patriots",16),(2014,"Patriots",16),(2015,"Patriots",16),(2016,"Patriots",16),(2017,"Patriots",16),(2018,"Patriots",16)]),
     testCase "yearAndTeamWithMoreThan1Tie should work" $ assertEqual [] (yearAndTeamWithMoreThan1Tie footballExampleStats) ([(1965,"Patriots"),(1966,"Patriots")]),
     testCase "yearAndTeamWithlessThan3Wins should work" $ assertEqual [] (yearAndTeamWithlessThan3Wins footballExampleStats) ([(1970,"Patriots"),(1981,"Patriots"),(1990,"Patriots"),(1992,"Patriots")]),
-    testCase "yearsAndTeamWithMoreWinsThanLosses should work" $ assertEqual [] (yearsAndTeamWithMoreWinsThanLosses footballExampleStats) ([(1961,"Patriots"),(1962,"Patriots"),(1963,"Patriots"),(1964,"Patriots"),(1966,"Patriots"),(1976,"Patriots"),(1977,"Patriots"),(1978,"Patriots"),(1979,"Patriots"),(1980,"Patriots"),(1982,"Patriots"),(1984,"Patriots"),(1985,"Patriots"),(1986,"Patriots"),(1987,"Patriots"),(1988,"Patriots"),(1994,"Patriots"),(1996,"Patriots"),(1997,"Patriots"),(1998,"Patriots"),(2001,"Patriots"),(2002,"Patriots"),(2003,"Patriots"),(2004,"Patriots"),(2005,"Patriots"),(2006,"Patriots"),(2007,"Patriots"),(2008,"Patriots"),(2009,"Patriots"),(2010,"Patriots"),(2011,"Patriots"),(2012,"Patriots"),(2013,"Patriots"),(2014,"Patriots"),(2015,"Patriots"),(2016,"Patriots"),(2017,"Patriots"),(2018,"Patriots")])
+    testCase "yearsAndTeamWithMoreWinsThanLosses should work" $ assertEqual [] (yearsAndTeamWithMoreWinsThanLosses footballExampleStats) ([(1961,"Patriots"),(1962,"Patriots"),(1963,"Patriots"),(1964,"Patriots"),(1966,"Patriots"),(1976,"Patriots"),(1977,"Patriots"),(1978,"Patriots"),(1979,"Patriots"),(1980,"Patriots"),(1982,"Patriots"),(1984,"Patriots"),(1985,"Patriots"),(1986,"Patriots"),(1987,"Patriots"),(1988,"Patriots"),(1994,"Patriots"),(1996,"Patriots"),(1997,"Patriots"),(1998,"Patriots"),(2001,"Patriots"),(2002,"Patriots"),(2003,"Patriots"),(2004,"Patriots"),(2005,"Patriots"),(2006,"Patriots"),(2007,"Patriots"),(2008,"Patriots"),(2009,"Patriots"),(2010,"Patriots"),(2011,"Patriots"),(2012,"Patriots"),(2013,"Patriots"),(2014,"Patriots"),(2015,"Patriots"),(2016,"Patriots"),(2017,"Patriots"),(2018,"Patriots")]),
+    
+    testCase "yearAndTeamWithTotalGames should work when empty" $ assertEqual [] ([]) (yearAndTeamWithTotalGames []), 
+    testCase "yearAndTeamWithMoreThan1Tie should work when empty" $ assertEqual [] ([]) (yearAndTeamWithMoreThan1Tie []),
+    testCase "yearAndTeamWithlessThan3Wins should work when empty" $ assertEqual [] ([]) (yearAndTeamWithlessThan3Wins []),
+    testCase "yearsAndTeamWithMoreWinsThanLosses should work when empty" $ assertEqual [] ([]) (yearsAndTeamWithMoreWinsThanLosses [])
+    
     ]
     
 ---- All Higher Order Test ----
