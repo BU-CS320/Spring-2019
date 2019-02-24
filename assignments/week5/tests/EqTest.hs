25f543f93d6c6fbca873aa6fd189453beb91f364
chantisnake <zcsx.ok@outlook.com>
2019-02-23 23:49:23 -0500

    change many tests to ==> style

--- assignments/week5/tests/EqTest.hs
+++ assignments/week5/tests/EqTest.hs
@@ -24,28 +24,18 @@ module EqTest where
     listTest = testGroup "Test equal List" [
 
       QC.testProperty "Eq Reflexivity List a == List a = True" $ 
-         \m -> m == (m :: List Integer),
-      
+         \m -> m == (m :: List Bool),
+          
       QC.testProperty "Eq Symmetry List (x==y) == (y==x)" $ 
-         \x y -> ((x:: List Integer) == (y :: List Integer)) == (y==x),
+         \x y -> ((x:: List Bool) == (y :: List Bool)) == (y==x),
     
-      QC.testProperty "Eq Transitivity List if x==y and y==z then x==z" $ 
-         \x y z -> if (((x:: List Integer)==(y :: List Integer)) && (y==(z :: List Integer))) == True
-                   then (x==z) == True
-                   else ((x==z) && (x==y)) == False,
-    
-      QC.testProperty "Eq Substitutivity List if a == b, then f a == f b" $ 
-         \x y -> if ((x :: List Integer) == (y :: List Integer)) == True
-                 then ((f x) == (f y)) == True
-                 else ((f x) == (f y)) == False,
-
       QC.testProperty "Eq Negation List x /= y = not (x == y)" $ 
-         \x y -> (x /= y) == not ((x :: List Integer) == (y:: List Integer))          
+         \x y -> (x /= y) == not ((x :: List Bool) == (y:: List Bool))          
       ]
 
-    f :: List Integer -> List Integer
-    f Nil = (Cons 1 Nil)
-    f (Cons a b) = Cons 1 ((Cons a b))
+    f :: List () -> List ()
+    f Nil = (Cons () Nil)
+    f (Cons a b) = Cons () ((Cons a b))
 
     fb :: List Bool -> List Bool
     fb Nil = (Cons False Nil)
@@ -55,131 +45,111 @@ module EqTest where
     maybeTest = testGroup "Test equal Maybe" [
 
       QC.testProperty "Eq Reflexivity Maybe a == Maybe a = True" $ 
-         \m -> m == (m ::  Maybe Integer),
+         \m -> m == (m ::  Maybe Bool),
       
       QC.testProperty "Eq Symmetry Maybe (x==y) == (y==x)" $ 
-         \x y -> ((x::  Maybe Integer) == (y ::  Maybe Integer))==(y==x),
+         \x y -> ((x::  Maybe Bool) == (y ::  Maybe Bool))==(y==x),
               
       QC.testProperty "Eq Transitivity Maybe if x==y and y==z then x==z" $ 
-         \x y z -> if (((x::  Maybe Integer)==(y ::  Maybe Integer)) && (y==(z ::  Maybe Integer))) == True
-                   then (x==z)==True
-                   else ((x==z) && (x==y)) ==False,
+         \x y z -> (((x::  Maybe ())==(y ::  Maybe ())) && (y==(z ::  Maybe ()))) ==> x==z,
     
       QC.testProperty "Eq Substitutivity Maybe if a == b, then f a == f b" $ 
-         \x y -> if ((x ::  Maybe Integer) == (y ::  Maybe Integer)) == True 
-                 then ((f2 x) == (f2 y)) == True
-                 else ((f2 x) == (f2 y)) == False,
+         \x y -> ((x ::  Maybe Bool) == (y ::  Maybe Bool)) ==> (f2 x) == (f2 y),
 
       QC.testProperty "Eq Negation Maybe x /= y = not (x == y)" $ 
-         \x y -> (x /= y) == not ((x ::  Maybe Integer) == (y::  Maybe Integer))          
+         \x y -> (x /= y) == not ((x ::  Maybe Bool) == (y::  Maybe Bool))          
       ]
 
-    f2 ::  Maybe Integer ->  Maybe Integer
+    f2 ::  Maybe Bool ->  Maybe Bool
     f2  Nothing =  Nothing
-    f2 ( Just a) =  Just (a+1)
+    f2 (Just a) =  Just (not a)
     
     --
     pairTest = testGroup "Test equal Pair" [
 
       QC.testProperty "Eq Reflexivity Pair a == Pair a = True" $ 
-         \m -> m == (m :: Pair Integer Integer),
+         \m -> m == (m :: Pair Bool Bool),
       
       QC.testProperty "Eq Symmetry Pair (x==y) == (y==x)" $ 
-         \x y -> ((x:: Pair Integer Integer) == (y :: Pair Integer Integer)) == (y==x),
+         \x y -> ((x:: Pair Bool Bool) == (y :: Pair Bool Bool)) == (y==x),
     
       QC.testProperty "Eq Transitivity Pair if x==y and y==z then x==z" $ 
-         \x y z -> if (((x:: Pair Integer Integer)==(y :: Pair Integer Integer)) && (y==(z :: Pair Integer Integer)))
-                   then (x==z)==True
-                   else ((x==z)&&(x==y))==False,
-    
+         \x y z -> (((x:: Pair Bool ())==(y :: Pair Bool ())) && (y==(z :: Pair Bool ())))==> (x==z),
+           
       QC.testProperty "Eq Substitutivity Pair if a == b, then f a == f b" $ 
-         \x y -> if ((x :: Pair Integer Integer) == (y :: Pair Integer Integer)) 
-                 then ((f3 x) == (f3 y)) == True
-                 else ((f3 x) == (f3 y)) == False,
+         \x y -> ((x :: Pair Bool Bool) == (y :: Pair Bool Bool)) ==> (f3 x) == (f3 y), 
 
       QC.testProperty "Eq Negation Pair x /= y = not (x == y)" $ 
-         \x y -> (x /= y) == not ((x :: Pair Integer Integer) == (y:: Pair Integer Integer))          
+         \x y -> (x /= y) == not ((x :: Pair Bool Bool) == (y:: Pair Bool Bool))          
       ]
 
-    f3 :: Pair Integer Integer -> Pair Integer Integer
-    f3 (Pair a b) = Pair (a+1) (b+1)
+    f3 :: Pair Bool Bool -> Pair Bool Bool
+    f3 (Pair a b) = Pair (not a) (not b)
     
     --
     idTest = testGroup "Test equal Id" [
 
       QC.testProperty "Eq Reflexivity Id a == Id a = True" $ 
-         \m -> m == (m :: Identity Integer),
+         \m -> m == (m :: Identity Bool),
       
       QC.testProperty "Eq Symmetry Id (x==y) == (y==x)" $ 
-         \x y -> ((x:: Identity Integer) == (y :: Identity Integer)) == (y==x),
+         \x y -> ((x:: Identity Bool) == (y :: Identity Bool)) == (y==x),
     
       QC.testProperty "Eq Transitivity Id if x==y and y==z then x==z" $ 
-         \x y z -> if (((x:: Identity Integer)==(y :: Identity Integer)) && (y==(z :: Identity Integer)))
-                   then (x==z)==True
-                   else ((x==z)&&(x==y))==False,
+         \x y z -> (((x:: Identity Bool)==(y :: Identity Bool)) && (y==(z :: Identity Bool))) ==> (x==z),
     
       QC.testProperty "Eq Substitutivity Id if a == b, then f a == f b" $ 
-         \x y -> if ((x :: Identity Integer) == (y :: Identity Integer)) 
-                 then ((f4 x) == (f4 y)) == True
-                 else ((f4 x) == (f4 y)) == False,
+         \x y -> ((x :: Identity Bool) == (y :: Identity Bool)) ==> (f4 x) == (f4 y),
 
       QC.testProperty "Eq Negation Id x /= y = not (x == y)" $ 
-         \x y -> (x /= y) == not ((x :: Identity Integer) == (y:: Identity Integer))          
+         \x y -> (x /= y) == not ((x :: Identity Bool) == (y:: Identity Bool))          
       ]
 
-    f4 :: Identity Integer -> Identity Integer
-    f4 (Identity a) = Identity (a+1)
+    f4 :: Identity Bool -> Identity Bool
+    f4 (Identity a) = Identity (not a)
 
 --
     eitherTest = testGroup "Test equal Either" [
 
       QC.testProperty "Eq Reflexivity Either a == Either a = True" $ 
-         \m -> m == (m ::  Either Integer Integer),
+         \m -> m == (m ::  Either Bool Bool),
       
       QC.testProperty "Eq Symmetry Either (x==y) == (y==x)" $ 
-         \x y -> ((x::  Either Integer Integer) == (y ::  Either Integer Integer)) == (y==x),
-    
+         \x y -> ((x::  Either Bool Bool) == (y ::  Either Bool Bool)) == (y==x),
+ 
       QC.testProperty "Eq Transitivity Either if x==y and y==z then x==z" $ 
-         \x y z -> if (((x::  Either Integer Integer)==(y ::  Either Integer Integer)) && (y==(z ::  Either Integer Integer)))
-                   then (x==z)==True
-                   else ((x==z)&&(x==y))==False,
-    
+         \x y z -> (((x::  Either Bool ())==(y ::  Either Bool ())) && (y==(z ::  Either Bool ()))) ==> (x==z),
+   
       QC.testProperty "Eq Substitutivity Either if a == b, then f a == f b" $ 
-         \x y -> if ((x ::  Either Integer Integer) == (y ::  Either Integer Integer)) 
-                 then ((f5 x) == (f5 y)) == True
-                 else ((f5 x) == (f5 y)) == False,
-
+         \x y -> ((x ::  Either Bool Bool) == (y ::  Either Bool Bool)) ==> ((f5 x) == (f5 y)),
+                
       QC.testProperty "Eq Negation Either x /= y = not (x == y)" $ 
-         \x y -> (x /= y) == not ((x ::  Either Integer Integer) == (y::  Either Integer Integer))          
+         \x y -> (x /= y) == not ((x ::  Either Bool Bool) == (y::  Either Bool Bool))          
       ]
 
-    f5 ::  Either Integer Integer ->  Either Integer Integer
-    f5 ( Left a) =  Left (a+1)
-    f5 ( Right b) =  Right (b+1)
+    f5 ::  Either Bool Bool ->  Either Bool Bool
+    f5 ( Left a) =  Left (not a)
+    f5 ( Right b) =  Right (not b)
 
     --
     triTest = testGroup "Test equal Trival" [
 
       QC.testProperty "Eq Reflexivity Trival a == Trival a = True" $ 
-         \m -> m == (m :: Trival Integer),
+         \m -> m == (m :: Trival Bool),
       
       QC.testProperty "Eq Symmetry Trival (x==y) == (y==x)" $ 
-         \x y -> ((x:: Trival Integer) == (y :: Trival Integer))==(y==x),
+         \x y -> ((x:: Trival Bool) == (y :: Trival Bool))==(y==x),
               
       QC.testProperty "Eq Transitivity Trival if x==y and y==z then x==z" $ 
-         \x y z -> if (((x:: Trival Integer)==(y :: Trival Integer)) && (y==(z :: Trival Integer))) == True
-                   then (x==z)==True
-                   else ((x==z) && (x==y)) ==False,
+         \x y z -> (((x:: Trival Bool)==(y :: Trival Bool)) && (y==(z :: Trival Bool))) ==> (x==z),
     
       QC.testProperty "Eq Substitutivity Trival if a == b, then f a == f b" $ 
-         \x y -> if ((x :: Trival Integer) == (y :: Trival Integer)) == True 
-                 then ((f6 x) == (f6 y)) == True
-                 else ((f6 x) == (f6 y)) == True,
+         \x y -> ((x :: Trival Bool) == (y :: Trival Bool)) ==> ((f6 x) == (f6 y)),
 
       QC.testProperty "Eq Negation Trival x /= y = not (x == y)" $ 
-         \x y -> (x /= y) == not ((x :: Trival Integer) == (y:: Trival Integer))          
+         \x y -> (x /= y) == not ((x :: Trival Bool) == (y:: Trival Bool))          
       ]
 
-    f6 :: Trival Integer -> Trival Integer
+    f6 :: Trival Bool -> Trival Bool
     f6 NoA = NoA
 
--- assignments/week5/tests/OrdTest.hs
+++ assignments/week5/tests/OrdTest.hs
@@ -17,91 +17,67 @@ module OrdTest where
         ]
     
     listTests = testGroup "Test for list datatype" [
-        QC.testProperty "Ord Transitivity List; if x <= y && y <= z = True, then x <= z = True" $ 
-            \x y z-> if (x:: List Integer)<=(y::List Integer) && y<=(z::List Integer)
-                     then x<=z
-                     else True, --not $ (x<=y) && (x<=z),
+        --QC.testProperty "Ord Transitivity List; if x <= y && 'y <= z = True, then x <= z = True" $ 
+        --    \x y z-> (x:: List Integer)<=(y::List Integer) && y<=(z::List Integer) ==> x<=z,
             
         QC.testProperty "Ord Reflexivity List; x<=x" $
-            \x -> (x::List Integer)<=x,
+            \x -> (x::List Integer)<=x
             
-        QC.testProperty "Ord Antisymmetry List; if x <= y && y <= x = True, then x == y = True" $
-            \x y-> if (x::List Integer)<=(y::List Integer) && y<=x 
-                   then x==y
-                   else not $ x==y
+        --QC.testProperty "Ord Antisymmetry List; if x <= y && y <= x = True, then x == y = True" $
+        --    \x y-> (x::List ())<=(y::List ()) && y<=x ==> x==y
         ]
     
     pairTests = testGroup "Test for pair datatype" [
             QC.testProperty "Ord Transitivity Pair; if x <= y && y <= z = True, then x <= z = True" $ 
-                \x y z-> if (x:: Pair Integer Integer)<=(y::Pair Integer Integer) && y<=(z::Pair Integer Integer)
-                         then x<=z
-                         else True,--not $ (x<=y) && (x<=z), 
+                \x y z-> (x:: Pair Bool Bool)<=(y::Pair Bool Bool) && y<=(z::Pair Bool Bool) ==> x<=z,
                 
             QC.testProperty "Ord Reflexivity Pair; x<=x" $
                 \x -> (x::Pair Integer Integer)<=x,
                 
             QC.testProperty "Ord Antisymmetry Pair; if x <= y && y <= x = True, then x == y = True" $
-                \x y-> if (x::Pair Integer Integer)<=(y::Pair Integer Integer) && y<=x 
-                       then x==y
-                       else not $ x==y
+                \x y-> (x::Pair Bool Bool)<=(y::Pair Bool Bool) && y<=x ==> x==y
         ]
         
     maybeTests = testGroup "Test for maybe datatype" [
             QC.testProperty "Ord Transitivity Maybe; if x <= y && y <= z = True, then x <= z = True" $ 
-                \x y z-> if (x:: Maybe Integer)<=(y::Maybe Integer) && y<=(z::Maybe Integer)
-                         then x<=z
-                         else True, 
+                \x y z-> (x:: Maybe Bool)<=(y::Maybe Bool) && y<=(z::Maybe Bool) ==> x<=z,
                 
             QC.testProperty "Ord Reflexivity Maybe; x<=x" $
                 \x -> (x::Maybe Integer)<=x,
                 
             QC.testProperty "Ord Antisymmetry Maybe; if x <= y && y <= x = True, then x == y = True" $
-                \x y-> if (x::Maybe Integer)<=(y::Maybe Integer) && y<=x 
-                       then x==y
-                       else not $ x==y
+                \x y-> (x::Maybe Bool)<=(y::Maybe Bool) && y<=x ==> x==y
         ]
     
     eitherTests = testGroup "Test for either datatype" [
             QC.testProperty "Ord Transitivity Either; if x <= y && y <= z = True, then x <= z = True" $ 
-                \x y z-> if (x:: Either Integer Integer)<=(y::Either Integer Integer) && y<=(z::Either Integer Integer)
-                         then x<=z
-                         else True,
+                \x y z-> (x:: Either Integer Integer)<=(y::Either Integer Integer) && y<=(z::Either Integer Integer) ==> x<=z,
                 
             QC.testProperty "Ord Reflexivity Either; x<=x" $
                 \x -> (x::Either Integer Integer)<=x,
                 
             QC.testProperty "Ord Antisymmetry Either; if x <= y && y <= x = True, then x == y = True" $
-                \x y-> if (x::Either Integer Integer)<=(y::Either Integer Integer) && y<=x 
-                       then x==y
-                       else not $ x==y
+                \x y->  (x::Either Bool Bool)<=(y::Either Bool Bool) && y<=x ==> x==y
         ]
     identityTests = testGroup "Test for Identity datatype" [
             QC.testProperty "Ord Transitivity Identity; if x <= y && y <= z = True, then x <= z = True" $ 
-                \x y z-> if (x:: Identity Integer)<=(y::Identity Integer) && y<=(z::Identity Integer)
-                         then x<=z
-                         else True, 
+                \x y z-> (x:: Identity Integer)<=(y::Identity Integer) && y<=(z::Identity Integer) ==> x<=z,
                 
             QC.testProperty "Ord Reflexivity Identity; x<=x" $
                 \x -> (x::Identity Integer)<=x,
                 
             QC.testProperty "Ord Antisymmetry Identity; if x <= y && y <= x = True, then x == y = True" $
-                \x y-> if (x::Identity Integer)<=(y::Identity Integer) && y<=x 
-                       then x==y
-                       else not $ x==y
+                \x y-> (x::Identity Bool)<=(y::Identity Bool) && y<=x  ==> x==y
         ]
     
     trivialTests = testGroup "Test for Trival datatype" [
             QC.testProperty "Ord Transitivity Trival; if x <= y && y <= z = True, then x <= z = True" $ 
-                \x y z-> if (x:: Trival Integer)<=(y::Trival Integer) && y<=(z::Trival Integer)
-                         then x<=z
-                         else True, 
+                \x y z-> (x:: Trival Integer)<=(y::Trival Integer) && y<=(z::Trival Integer) ==> x<=z,
                 
             QC.testProperty "Ord Reflexivity Trival; x<=x" $
                 \x -> (x::Trival Integer)<=x,
                 
             QC.testProperty "Ord Antisymmetry Trival; if x <= y && y <= x = True, then x == y = True" $
-                \x y-> if (x::Trival Integer)<=(y::Trival Integer) && y<=x 
-                       then x==y
-                       else not $ x==y
+                \x y-> (x::Trival Bool)<=(y::Trival Bool) && y<=x  ==> x==y
         ]
         
