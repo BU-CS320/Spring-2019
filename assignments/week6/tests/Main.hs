05e58c5446569ef6e08eebda0d0e4068932a20f7
Mark <lemay@bu.edu>
2019-02-27 13:34:00 -0500

    HW5

---
+++ assignments/week6/tests/Main.hs
@@ -0,0 +1,21 @@
+module Main where
+
+  import System.Environment
+  import Test.Tasty (defaultMain, testGroup, TestTree)
+
+  import LangTest (langTest)
+
+
+  main = 
+    do 
+        setEnv "TASTY_TIMEOUT" "5s"
+        setEnv "TASTY_QUICKCHECK_TESTS" "20" --TODO: I never trust less than 10000
+        setEnv "TASTY_QUICKCHECK_MAX_SIZE" "50"
+        defaultMain allTests
+        unsetEnv "TASTY_TIMEOUT"
+        unsetEnv "TASTY_QUICKCHECK_TESTS"
+        unsetEnv "TASTY_QUICKCHECK_MAX_SIZE"
+
+  allTests = testGroup "all tests" [
+    langTest
+    ]
