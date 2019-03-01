f7a7ad762be1c053fc13900ec8859a192276a4f8
chantisnake <zcsx.ok@outlook.com>
2019-02-28 21:36:06 -0500

    add tests

--- assignments/week6/tests/Main.hs
+++ assignments/week6/tests/Main.hs
@@ -2,20 +2,27 @@ module Main where
 
   import System.Environment
   import Test.Tasty (defaultMain, testGroup, TestTree)
+  import Test.Tasty.HUnit (assertEqual, assertBool, testCase, (@=?))
+  import Test.Tasty.QuickCheck (testProperty,Arbitrary, oneof,arbitrary )
 
-  import LangTest (langTest)
-
+  import LangTest1 (langTest1)
+  import LangTest2 (langTest2)
+  import LangTest3 (langTest3)
+  import LangTest4 (langTest4)
 
   main = 
     do 
         setEnv "TASTY_TIMEOUT" "5s"
-        setEnv "TASTY_QUICKCHECK_TESTS" "20" --TODO: I never trust less than 10000
+        setEnv "TASTY_QUICKCHECK_TESTS" "1000" --TODO: I never trust less than 10000
         setEnv "TASTY_QUICKCHECK_MAX_SIZE" "50"
-        putStrLn "\nTest is not avaliable yet\n"
+        defaultMain allTests
         unsetEnv "TASTY_TIMEOUT"
         unsetEnv "TASTY_QUICKCHECK_TESTS"
         unsetEnv "TASTY_QUICKCHECK_MAX_SIZE"
 
   allTests = testGroup "all tests" [
-    -- langTest
+    langTest1,
+    langTest2,
+    langTest3,
+    langTest4
     ]
