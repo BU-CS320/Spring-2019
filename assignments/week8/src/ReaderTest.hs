86ef502a06e58dc770b231b1164eca9e565ad6f2
Wayne Snyder <waysnyder@gmail.com>
2019-03-22 18:12:54 -0400

    Add files via upload

---
+++ assignments/week8/src/ReaderTest.hs
@@ -0,0 +1,48 @@
+import Prelude
+import Data.Map (Map)-- for Env
+import qualified Data.Map as Map
+import Reader
+
+
+-- Examples and simple test cases
+
+
+type Env = Map String Integer
+
+environment :: Env      
+environment = Map.fromList([("x",4),("y",7)])
+
+prog :: Reader Env Integer
+prog = do e <- ask
+          case Map.lookup "x" e of
+             Nothing -> return 0
+             Just n  -> return n
+
+test :: String -> Reader Env Integer
+test x  = do env <- ask
+             return (Map.findWithDefault 0 x env)
+
+ex1 = runReader prog environment
+
+ex2 = runReader prog Map.empty
+
+ex3 = runReader (test "y") environment
+
+ex4 = runReader (test "z") environment
+
+{-
+
+Reader> ex1
+4
+Reader> ex2
+0
+Reader> ex3
+7
+Reader> ex4
+0
+
+-}
+
+
+ 
+  
