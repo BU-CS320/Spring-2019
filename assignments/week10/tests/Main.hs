57bd2cfa28596f7b58fb810cd95469bc23c21739
chantisnake <zcsx.ok@outlook.com>
2019-04-09 10:20:24 -0400

    add the test for type class law

--- assignments/week10/tests/Main.hs
+++ assignments/week10/tests/Main.hs
@@ -5,10 +5,11 @@ module Main where
 
   import LangParserTest (parserTest)
   import LangEvalTest (errorTest, evalTest, stdLibTest)
+  import EnvUnsafeTest (monadLawTest, functorLawTest)
 
   main = 
     do 
-        setEnv "TASTY_TIMEOUT" "20s"
+        setEnv "TASTY_TIMEOUT" "40s"
         setEnv "TASTY_QUICKCHECK_TESTS" "1000" --TODO: I never trust less than 10000
         setEnv "TASTY_QUICKCHECK_MAX_SIZE" "50"
         defaultMain allTests
@@ -20,6 +21,8 @@ module Main where
     testGroup
       "allTests"
       [
+        functorLawTest,
+        monadLawTest,
         errorTest,
         evalTest,
         stdLibTest,
