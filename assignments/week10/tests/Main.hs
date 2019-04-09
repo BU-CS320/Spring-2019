facb29a7cc0074c9e54f913a025c99192acd66cc
chantisnake <zcsx.ok@outlook.com>
2019-04-09 00:23:32 -0400

    add test for eval

--- assignments/week10/tests/Main.hs
+++ assignments/week10/tests/Main.hs
@@ -4,12 +4,13 @@ module Main where
   import Test.Tasty (defaultMain, testGroup, TestTree)
 
   import LangParserTest (parserTest)
+  import LangEvalTest (errorTest, evalTest, stdLibTest)
 
   main = 
     do 
         setEnv "TASTY_TIMEOUT" "5s"
-        setEnv "TASTY_QUICKCHECK_TESTS" "20" --TODO: I never trust less than 10000
-        setEnv "TASTY_QUICKCHECK_MAX_SIZE" "50"
+        setEnv "TASTY_QUICKCHECK_TESTS" "1000" --TODO: I never trust less than 10000
+        setEnv "TASTY_QUICKCHECK_MAX_SIZE" "100"
         defaultMain allTests
         unsetEnv "TASTY_TIMEOUT"
         unsetEnv "TASTY_QUICKCHECK_TESTS"
@@ -19,5 +20,8 @@ module Main where
     testGroup
       "allTests"
       [
+        errorTest,
+        evalTest,
+        stdLibTest,
         parserTest
       ]
